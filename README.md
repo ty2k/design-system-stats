a7e887bbff53cbd6d2ff5e7ee0b55e300394cd94
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-07-23 16:34:52 -0700

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+```sh
+# Save the results to a file
+bash find-adopters.sh > adopting-repos.txt
+```
---
+++ find-adopters.sh
@@ -0,0 +1,17 @@
+# find-adopters.sh
+
+#!/usr/bin/env bash
+# Requires: gh CLI (authenticated), jq
+# Usage: bash find-adopters.sh
+
+ORG="bcgov"
+PACKAGE="@bcgov/design-system-react-components"
+
+echo "Searching for repos in '$ORG' that use '$PACKAGE'..."
+
+gh search code "$PACKAGE" \
+  --owner "$ORG" \
+  --filename package.json \
+  --json repository \
+  --jq '[.[].repository.nameWithOwner] | unique | sort[]' \
+  --limit 100

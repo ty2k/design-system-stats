5eba06b08262baf0d4157aed0c83c9286c4ef282
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-08-17 15:08:57 -0700

    Generate React version information summary

--- generate-summary-table.sh
+++ generate-summary-table.sh
@@ -2,7 +2,7 @@
 
 jq -r '
   # Flatten to [{repo, component, count}]
-  [ to_entries[] | .key as $repo | .value | to_entries[] |
+  [ to_entries[] | .key as $repo | (.value.components // .value) | to_entries[] |
     { repo: $repo, component: .key, count: (.value.instances | length) }
   ]
   | group_by(.component)[]
@@ -13,3 +13,16 @@ jq -r '
 | sort -t$'\t' -k2 -rn \
 | awk 'BEGIN { print "| Component | Total Instances | Repos Using It |"; print "| --- | --- | --- |" }
        { printf "| %s | %s | %s |\n", $1, $2, $3 }'
+
+echo ""
+echo "## React Versions"
+echo ""
+jq -r '
+  [ to_entries[] | .key as $repo | .value.reactVersions[]? |
+    [$repo, .packagePath, .section, .version] | @tsv
+  ]
+  | .[]
+' adoption-report.json \
+| sort -t$'\t' -k4 -V \
+| awk 'BEGIN { print "| Repository | Package | Section | React Version |"; print "| --- | --- | --- | --- |" }
+       { printf "| %s | %s | %s | %s |\n", $1, $2, $3, $4 }'
